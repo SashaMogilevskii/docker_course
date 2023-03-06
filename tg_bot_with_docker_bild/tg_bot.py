f2c7dff1ec9642fa15bb37f7811c70369377c999
Sasha_Mogilevskii <mogilevskiy94@mail.ru>
2023-03-06 11:50:49 -0300

    change folders.

--- tg_bot_with_docker_bild/tg_bot.py
+++ tg_bot_with_docker_bild/tg_bot.py
@@ -1,20 +1,33 @@
 import logging
 import os
 
+from clickhouse_driver import Client
 import pandas as pd
 from aiogram import Bot, Dispatcher, executor, types
 from dotenv import load_dotenv
-
 logging.basicConfig(
     format="%(levelname)s: %(asctime)s - %(message)s",
     datefmt="%d-%b-%y %H:%M:%S",
     level=logging.INFO
 )
 
-# your tg_bot token
+# your tg_bot tokenS
 load_dotenv()
-SECRET_TOKEN = os.getenv("SECRET_TOKEN")
+try:
+    SECRET_TOKEN = os.getenv("SECRET_TOKEN")
+except:
+    SECRET_TOKEN = os.environ.get("SECRET_TOKEN")
 assert SECRET_TOKEN is not None, "SECRET_TOKEN IS EMPTY!"
+
+connection = Client(
+    host="localhost",
+    user="default",
+    password="",
+    port=9000,
+    database="todo"
+)
+
+
 
 PATH_TO_TODO_TABLE = "todo_result/todo_list.csv"
 
@@ -22,40 +35,25 @@ bot = Bot(token=SECRET_TOKEN)
 dp = Dispatcher(bot)
 
 
-def get_todo_data() -> pd.DataFrame:
-    """
-    :return: DataFrame with our data (csv)
-    """
-    return pd.read_csv(PATH_TO_TODO_TABLE)
-
-
 @dp.message_handler(commands="all")
 async def all_tasks(payload: types.Message):
 
+    ch_all_data = connection.execute("SELECT * FROM todo.todo")
 
-    try:
-        await payload.reply(f"```{get_todo_data().to_markdown()}```",
-                            parse_mode="Markdown"
-                            )
-    except:
-        await payload.reply(f"ToDo list is empty!",
-                            parse_mode="Markdown"
-                            )
-
-
-
+    await payload.reply(
+        f"```{pd.DataFrame(ch_all_data, columns=['text', 'status']).to_markdown()}```",
+        parse_mode="Markdown"
+    )
 
 @dp.message_handler(commands="add")
 async def add_tasks(payload: types.Message):
     text = payload.get_args().strip()
-    new_task = pd.DataFrame({"text": [text],
-                             "status": ["active"]
-                             })
-    updates_tasks = pd.concat([get_todo_data(),
-                               new_task],
-                              ignore_index=True, axis=0)
-    # Save
-    updates_tasks.to_csv(PATH_TO_TODO_TABLE, index=False)
+
+    connection.execute(
+        "INSERT INTO todo.todo (text, status) VALUES (%(text)s, %(status)s)",
+        {"text":text, "status":"active"}
+
+    )
 
     logging.info(f"Add new task - {text}")
     await payload.reply(f"Add new task - {text}",
@@ -65,12 +63,11 @@ async def add_tasks(payload: types.Message):
 @dp.message_handler(commands="done")
 async def complete_task(payload: types.Message):
     text = payload.get_args().strip()
-    df = get_todo_data()
-    df.loc[df.text == text, "status"] = "complete"
 
-    # Save
-    df.to_csv(PATH_TO_TODO_TABLE, index=False)
-
+    connection.execute(
+        "ALTER TABLE todo.todo UPDATE status = 'complete' WHERE text = %(text)s",
+        {"text": text}
+    )
     logging.info(f"Finished task - {text}")
     await payload.reply(f"Complete *{text}*",
                         parse_mode="Markdown")
